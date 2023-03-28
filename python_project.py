36038463df4887fb7070927dbb9d408f53e8f4c2
sUnnykumar2210 <123459903+sUnnykumar2210@users.noreply.github.com>
2023-03-28 10:37:07 +0530

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -7,7 +7,7 @@ root.title("Scientific Calculator")
 root.configure(background = 'white')
 root.resizable(width=False, height=False)
 root.geometry("480x568+450+90")
-calc = Frame(root)
+calc = Frame(root)                          #edited
 calc.grid()
 
 class Calc():
